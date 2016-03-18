081a100ac48df7a25676f1a80f2d2be087f16993
konapun <bremen.braun@gmail.com>
2016-03-18 18:38:02 -0500

    Started working on sequence glyph

--- GenomeBrowser/Glyph/Sequence.php
+++ GenomeBrowser/Glyph/Sequence.php
@@ -2,20 +2,43 @@
 namespace GenomeBrowser\Glyph;
 
 class Sequence extends Glyph {
-  private $thickness;
+  private $sequence;
+  private $colorMap;
+  private $textHeight;
+  private $padding;
 
-  function __construct($track, $start, $end) {
-    parent::__construct($track, $start, $end);
-    $this->thickness = 3;
+  function __construct($track, $start, $sequence) {
+    parent::__construct($track, $start, ($start+strlen($sequence)-1));
+    $this->sequence = $sequence;
+    $this->textHeight = 12;
+    $this->colorMap = array(
+      'A' => array(
+        'bg' => 'blue',
+        'fg' => 'white'
+      ),
+      'C' => array(
+        'bg' => 'red',
+        'fg' => 'white'
+      ),
+      'T' => array(
+        'bg' => 'yellow',
+        'fg' => 'black'
+      ),
+      'G' => array(
+        'bg' => 'orange',
+        'fg' => 'black'
+      )
+    );
   }
 
   function getRenderSize() {
-    return $this->thickness;
+    return $this->textHeight + ($padding*2);
   }
 
   function render() {
     $drawVertical = $this->drawsVertical();
 
+    $cellSize = $this->getSize() / strlen($this->sequence);
     $trackCenter = $this->getSize() / 2;
     $strokeWidth = $this->getSize();
     list($start, $end) = $this->getCoordinates();
@@ -32,6 +55,15 @@ class Sequence extends Glyph {
       $y2 = $end;
     }
 
+    $x = 0;
+    $y = 0;
+    $svg = array('<g class="sequence">');
+    foreach (str_split($this->sequence) as $base) {
+      array_push($svg, '<rect x="'.$x.'" y="'.$y.'" width="'.$cellSize.'" height="'.$this->getRenderSize().'" fill="'.$this->colorMap[strtoupper($base)]['bg'].'"/>');
+      $x += $cellSize;
+    }
+    array_push($svg, '</g>');
+    return join("\n", $svg);
     return '<line class="line-glyph" stroke-width="'.$strokeWidth.'" stroke="'.$this->getColor().'" x1="'.$x1.'" y1="'.$y1.'" x2="'.$x2.'" y2="'.$y2.'"/>';
   }
 }
--- test/glyphs.php
+++ test/glyphs.php
@@ -5,7 +5,7 @@ use GenomeBrowser\Browser as Browser;
 $browser = new Browser(900);
 
 $geneModelTrack = $browser->addTrack();
-$geneModel = $geneModelTrack->addGeneModel(1, 200);
+$geneModel = $geneModelTrack->addGeneModel(1, 150);
 $geneModel->addTranscript(20, 30);
 $geneModel->addTranscript(50, 90);
 $geneModel->setLabel('gene-A');
@@ -17,7 +17,9 @@ $exonTrack->addArrow(90, 50);
 $testTrack = $browser->addTrack();
 $testTrack->addFeature('arrow', 1, 100)->setColor('orange');
 
-//$browser->addTrack()->addFeature('sequence', 10, "ACTGACTGACTGACTGACTGACTG");
+$sequence = $browser->addTrack()->addFeature('sequence', 10, "
+ACTGACTGACTGACTGACTGACTGACTGACTGACTGACTGACTGACTGACTGACTGACTGACTGACTGACTGACTGACTGACTGACTGACTGACTGACTGACTGACTGACTGACTGACTGACTGACTGACTGACTGACTGACTGAAAAA");
+
 $scale = $browser->addTrack()->addScale();
 $scale->labelSteps();
 $scale->setStepping(50);
